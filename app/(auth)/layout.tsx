d8e78766bf67d021843d45c1e8b7a97ab8d6397f
pavandantu18 <pavankumar.reddy@uhnder.com>
2023-11-28 22:47:30 +0530

    routing and basic authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </main>
+  );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">next js 13 we are coming</h1>
-    </div>
-  );
-}
